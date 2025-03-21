c57a3787b2e0d869cee796bd139fc61ff653db8a
joao salgueiro <jp-siqueira@outlook.com>
2025-03-21 17:31:39 -0300

    Adding the learn-more page and improving the website style, also using partials now

--- controlx/urls.py
+++ controlx/urls.py
@@ -14,10 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from main import views
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+
+
+from controlx import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-]
+    path('learn_more/', views.learn_more, name='learn_more'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
