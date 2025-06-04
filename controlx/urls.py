c4ad12f67b130dda0387da9fa128e1d0be88275d
joao salgueiro <jp-siqueira@outlook.com>
2025-06-04 10:40:47 -0300

    Settings and DEBUG config

--- controlx/urls.py
+++ controlx/urls.py
@@ -31,7 +31,4 @@ urlpatterns = [
     path('new_post/', views.new_post, name='new_post'),
     path('edit_post/<int:post_id>/', views.edit_post, name='edit_post'),
     path('delete_post/<int:post_id>/', views.delete_post, name='delete_post'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
